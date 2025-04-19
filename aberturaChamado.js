d854367af18453a9f144746dc909108ce06c5f64
NicolasMatheusFerreira <nicolasmatheusferreira13@gmail.com>
2025-04-18 21:27:42 -0300

    Readequação da estrutura dos arquivos

--- aberturaChamado.js
+++ aberturaChamado.js
@@ -49,6 +49,8 @@ function validaEmail(email) {
     return false;
 }
 
+email.addEventListener('blur', () => {email.style.border = '1px solid #888';});
+
 nome.addEventListener('input', () => {
     if (validaNome())
         nome.style.border = '2px solid rgb(60, 230, 54)';
@@ -60,6 +62,8 @@ function validaNome() {
         return false
     return true;
 }
+
+nome.addEventListener('blur', () => {nome.style.border = '1px solid #888';});
 
 function validaTelefone() {
     const telefoneRegex = new RegExp(/^[0-9]{10,}$/);
@@ -75,8 +79,10 @@ telefone.addEventListener('input', () => {
     else telefone.style.border = '2px solid #e63636';
 });
 
+telefone.addEventListener('blur', () => {telefone.style.border = '1px solid #888';});
+
 function validaCelular() {
-    const celularRegex = new RegExp(/^[0-9]{10,}$/);
+    const celularRegex = new RegExp(/^[0-9]{11,}$/);
 
     if (celularRegex.test(celular.value))
         return true;
@@ -88,6 +94,8 @@ celular.addEventListener('input', () => {
         celular.style.border = '2px solid rgb(60, 230, 54)';
     else celular.style.border = '2px solid #e63636';
 });
+
+celular.addEventListener('blur', () => {celular.style.border = '1px solid #888';});
 
 function validaCampos() {
    if (!validaNome() || (!validaEmail(email.value) && email.value>"") || !validaTelefone() || !validaCelular()) {
@@ -143,7 +151,7 @@ const categoriaProblema = `<select name="categoriaProblema" id="categoriaProblem
                             <option value="selecione" label="Selecione" class="cx-entrada" selected>-- SELECIONE --</option>
                             <option value="nao-faz-chamadas" class="cx-entrada">(1). (Inoperância Parcial) - Não Faz Chamadas</option>
                             <option value="nao-recebe-chamadas" class="cx-entrada">(2). (Inoperância Parcial) - Não Recebe Chamadas</option>
-                            <option value="nao-faz-nao-recebe-chamadas" class="cx-entrada">(3). (Inoperância Total) - Não Faz E Não Chamadas</option>
+                            <option value="nao-faz-nao-recebe-chamadas" class="cx-entrada">(3). (Inoperância Total) - Não Faz E Não recebe Chamadas</option>
                             <option value="problemas-audio" class="cx-entrada">(4). Problemas no Áudio das Chamadas (Voz Robótizada, Picotamento de Voz)</option>
                             <option value="configuracao-equipamento" class="cx-entrada">(5). Configuração de Equipamentos</option>
                             <option value="ajustes" class="cx-entrada">(6). Criação de Contas</option>
@@ -195,6 +203,8 @@ categoriaServico.addEventListener('click', () => {
                 containerProblema.innerHTML = configuracaoDeEquipamentos;
             }
         });
+    } else if (valorSelecionadoCategoriaServico=="stfc") {
+
     }
 });
 
